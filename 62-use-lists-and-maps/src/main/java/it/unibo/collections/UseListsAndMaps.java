94fd7682e1f0e356edb53ad3cb65ec132ea51f1c
Khiba01 <49270495+Khiba01@users.noreply.github.com>
2022-11-26 18:15:38 +0100

    Esercizio 1 quasi fatto, manca Map

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,7 +1,11 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
@@ -21,19 +25,29 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        List<Integer> arrayList = new ArrayList<>();
+        final int MAX_POPULATION = 1000;
+        for (int i = 0; i < MAX_POPULATION; i++) {
+            arrayList.add(i + 1000);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        List<Integer> linkedList = new LinkedList<>(arrayList);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        final int last = arrayList.get(arrayList.size() - 1);
+        arrayList.set(arrayList.size() - 1, arrayList.get(0));
+        arrayList.set(0, last);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        System.out.println(arrayList);
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +55,25 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        long time = 0;
+        final int NEW_ELEMENTS = 100_000;
+        
+        test1(time, NEW_ELEMENTS, arrayList);
+
+        test1(time, NEW_ELEMENTS, linkedList);
+
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example PerfTest.java.
          */
+        final int READS = 1000;
+        
+        test2(time, READS, arrayList);
+
+        test2(time, READS, linkedList);
+        
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -66,5 +93,36 @@ public final class UseListsAndMaps {
         /*
          * 8) Compute the population of the world
          */
+    }
+    public static long prepareVariable() {
+        long time = System.nanoTime();
+        return time;
+    }
+    public static void computeAndResolve(long time, List<Integer> set) {
+        time = System.nanoTime() - time;
+        final var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(// NOPMD
+            "Converting "
+                + set.size()
+                + " ints to String and inserting them in a Set took "
+                + time
+                + "ns ("
+                + millis
+                + "ms)"
+        );
+    }
+    public static void test1 (long time,int index, List<Integer> col) {
+        time = prepareVariable();
+        for (int i = 0; i < index; i++) {
+            col.add(0, i);
+        }
+        computeAndResolve(time, col);
+    }
+    public static void test2 (long time, int index, List<Integer> col) {
+        time = prepareVariable();
+        for (int i = 0; i < index; i++) {
+            col.get(col.size() / 2);
+        }
+        computeAndResolve(time, col);
     }
 }
