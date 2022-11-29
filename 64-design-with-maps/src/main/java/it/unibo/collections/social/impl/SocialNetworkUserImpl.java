ca08e50b01bc6bb04b2097fec2b7dcd228e5cc93
Karim El Kholy - karim.elkholy@studio.unibo.it <karim.elkholy@studio.unibo.it>
2022-11-29 17:07:40 +0100

    Esercizio 3, null pointer exeption

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -11,6 +11,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -36,6 +37,7 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      * In order to save the people followed by a user organized in groups, adopt
      * a generic-type Map:  think of what type of keys and values would best suit the requirements
      */
+    private Map<String, List<U>> followerMap = new HashMap<>();
 
     /*
      * [CONSTRUCTORS]
@@ -62,13 +64,15 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *            application
      */
     public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(null, null, null, 0);
+        super(name, surname, user, userAge);
     }
 
     /*
      * 2) Define a further constructor where the age defaults to -1
      */
-
+    public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+        super(name, surname, user, -1);
+    }
     /*
      * [METHODS]
      *
@@ -76,6 +80,12 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public boolean addFollowedUser(final String circle, final U user) {
+        final List<U> currentFollower = new ArrayList<>(followerMap.get(circle));
+        if (!followerMap.values().contains(user)) {
+            currentFollower.add(user);
+            followerMap.put(circle, currentFollower);
+            return true;
+        }
         return false;
     }
 
@@ -86,11 +96,14 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
+        if (followerMap.containsKey(groupName)) {
+            return followerMap.get(groupName);     
+        }
+        return new ArrayList<>();
     }
 
     @Override
     public List<U> getFollowedUsers() {
-        return null;
+        return followerMap.get(this.getUsername());
     }
 }
